48e5abfe1d7ffcdf2ba4597bc7c6a444de945e67
dhruv jain <dhruvjain2424@gmail.com>
2024-08-24 12:52:39 +0530

    added

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@ import './App.css'
 import Contact from './sections/Contact/Contact'
 import Footer from './sections/Footer/Footer'
 import Hero from './sections/Hero/Hero'
-import Projects from './sections/projects/Projects'
+import Projects from './sections/Projects/Projects'
 import Skills from './sections/Skills/Skills'
 
 function App() {
